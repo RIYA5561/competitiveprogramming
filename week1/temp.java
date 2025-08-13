d432960390db947a93c29f3e927b7f30a662a548
RIYA5561 <agrawalriya868@gmail.com>
2025-08-13 11:12:38 +0530

    Create temp.java

---
+++ week1/temp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class basic{
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter temperature:");
+    int temp = sc.nextInt(); 
+;    int fahrenheit = (temp * 9/5) + 32;
+    System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+    
+    }}
