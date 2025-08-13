7250932c81ce7dc20a1acf8364a781cfa9e83086
RIYA5561 <agrawalriya868@gmail.com>
2025-08-13 11:15:42 +0530

    Create area.java

---
+++ week1/area.java
@@ -0,0 +1,5 @@
+Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the radius of the circle:");
+    int radius = sc.nextInt();
+    double area = Math.PI * radius * radius;
+    System.out.println(area);
