a372d0dec4a9801d84632cd684add75bdd492d16
RIYA5561 <agrawalriya868@gmail.com>
2025-08-19 11:20:59 +0530

    Create ques9.java

---
+++ week21/ques9.java
@@ -0,0 +1,16 @@
+
+
+public class account {
+     //Create a class Account with instance variables: accountHolder, balance. Use default values. Print them without setting any values.
+    String accountHolder;
+    double balance;
+    public static void main(String[] args) {
+        account acc = new account();
+        System.out.println("Account Holder: " + acc.accountHolder);
+        System.out.println("Balance: " + acc.balance);
+    }
+    
+    
+}
+ans=null
+    0.0
