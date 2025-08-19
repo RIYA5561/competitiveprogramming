8e0195ad3e00f92d28cb94b6d379ba84d5a6d42c
RIYA5561 <agrawalriya868@gmail.com>
2025-08-19 10:18:44 +0530

    Create QUES3.java

---
+++ week21/QUES3.java
@@ -0,0 +1,10 @@
+class method{
+    void show(){
+        int num =30;
+        System.out.println("The value of num is: " + num);
+    }
+    public static void main(String[] args) {
+        method obj = new method();
+        obj.show();
+    }
+}
