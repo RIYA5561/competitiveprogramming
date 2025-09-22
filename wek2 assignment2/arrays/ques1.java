2ef9844e5703577a523223c9bf73e8d51e567091
RIYA5561 <agrawalriya868@gmail.com>
2025-09-22 09:37:55 +0530

    Update ques1.java

--- wek2 assignment2/arrays/ques1.java
+++ wek2 assignment2/arrays/ques1.java
@@ -2,4 +2,19 @@ package wek2 assignment2.arrays;
 
 public class ques1 {
     
+//1 Two Sum
+public class Question_1 {
+	 public int[] twoSum(int[] nums, int target) {
+	        HashMap<Integer,Integer> hm = new HashMap<>();
+	        for(int i=0;i<nums.length;i++){
+	            int k = target - nums[i];
+	            if(hm.containsKey(k)){
+	                return new int[]{hm.get(k),i};
+	            }
+	            hm.put(nums[i],i);
+	        }
+	        return null;
+	    }
 }
+    
+
